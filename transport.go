44a60db4ed357f8fdec78e80391534aeea1faa24
LeeCQ <lee-cq@qq.com>
2023-06-29 10:59:19 +0800

    v2-Alpha1: 使用JSON配置文件完成多接口配置。

--- transport.go
+++ transport.go
@@ -3,42 +3,125 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"net"
+	"os"
 	"time"
 
 	"tailscale.com/tsnet"
 )
 
+type Transport struct {
+	RemotePort string `json:"remotePort"`
+	LocalPort  string `json:"localPort"`
+}
+
+type Config struct {
+	Hostname  string `json:"hostname"`
+	Authkey   string `json:"authkey"`
+	Ephemeral bool   `json:"ephemeral"`
+	Dir       string `json:"dir"`
+
+	Transports []Transport `json:"transports"`
+}
+
 var (
-	remotePort = flag.String("remotePort", "nil", "tailscale中的远程端口, 格式: <ip|Hostname>:port")
-	localPort  = flag.String("localPort", "nil", "本地需要被监听的端口，格式 [ip|hostname]:port")
-	hostname   = flag.String("hostname", "tsnet-translate", "hostname to use on the tsnet")
+	config     = Config{}
+	configFile = flag.String("c", "config.json", "配置文件")
+	newFile    = flag.Bool("n", false, "在当前目录中创建模板")
 )
 
-func main() {
+func createConfig() {
+	dir, _ := os.Getwd()
+	config := Config{
+		Hostname:  "注册在TSNET中的主机名",
+		Authkey:   "认证密钥",
+		Ephemeral: false,
+		Dir:       "文件存储位置",
+	}
+	t := Transport{
+		RemotePort: "RemoteIP:port",
+		LocalPort:  "LocalIP:port",
+	}
+	config.Transports = append(config.Transports, t)
 
+	jsonBytes, _ := json.Marshal(config)
+
+	println("在 %s 目录下", dir)
+	err := os.WriteFile("config.json", jsonBytes, 'w')
+	if err != nil {
+		fmt.Println("Write Error")
+	}
+}
+
+func GetConfig() {
 	flag.Parse()
 
-	fmt.Println("初始化 tsnet.Server")
-	tsServer := new(tsnet.Server)
-	tsServer.Hostname = *hostname
+	if *newFile {
+		createConfig()
+		os.Exit(0)
+	}
 
+	jsonBytes, err := os.ReadFile(*configFile)
+	if err != nil {
+		fmt.Printf("无效的配置文件或路径: %v \n", err)
+		os.Exit(1)
+	}
+
+	err = json.Unmarshal(jsonBytes, &config)
+	if err != nil {
+		fmt.Printf("Json 解析失败 %v \n", err)
+		os.Exit(1)
+	}
+}
+
+func main() {
+	// 初始化配置
+
+	defer fmt.Println("All End ...")
+
+	GetConfig()
+
+	fmt.Println("初始化 tsnet.Server, 配置相关服务")
+	tsServer := new(tsnet.Server)
+	tsServer.Hostname = config.Hostname
+	tsServer.Ephemeral = config.Ephemeral
+	if config.Authkey != "" {
+		tsServer.AuthKey = config.Authkey
+	}
+	if config.Dir != "" {
+		tsServer.Dir = config.Dir
+	}
 	defer fmt.Println("tsnet Server a has topped .")
 	defer func(tsServer *tsnet.Server) {
 		err := tsServer.Close()
 		if err != nil {
+			fmt.Println("Tsnet Close Error.")
 			return
 		}
 	}(tsServer)
+	// 配置tsServer完成 ...
 
 	fmt.Println("开始链接至tsnet")
 	if err := tsServer.Start(); err != nil {
 		return
 	}
-	fmt.Println("监听端口 8888")
-	listener, err := net.Listen("tcp", *localPort)
+	// tsServer完成
+	for _, transport := range config.Transports {
+		go Transporter(tsServer, transport.RemotePort, transport.LocalPort)
+	}
+	defer fmt.Println("Ok, Will be Done ... ")
+
+	for {
+		time.Sleep(time.Minute)
+	}
+}
+
+func Transporter(tsServer *tsnet.Server, remotePort string, localPort string) {
+	fmt.Println("监听端口", localPort)
+	listener, err := net.Listen("tcp", localPort)
 	if err != nil {
 		fmt.Println("Error: ", err)
 		return
@@ -56,7 +139,7 @@ func main() {
 
 		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 
-		tsConn, err := tsServer.Dial(ctx, "tcp", *remotePort)
+		tsConn, err := tsServer.Dial(ctx, "tcp", remotePort)
 		if err != nil {
 			cancel()
 			fmt.Println("Error: ", err)
